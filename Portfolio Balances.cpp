934abf837e3b4bbbd308830adf6ea8d7489637dd
Shereya Philip <pvshereya@gmail.com>
2023-09-26 16:21:09 +0530

    Update Portfolio Balances.cpp

--- Portfolio Balances.cpp
+++ Portfolio Balances.cpp
@@ -1,18 +1,21 @@
-long maxValue(int n, vector<vector<int>> rounds) {
+//cumulative sum approach
+long maxValue(int n, vector<vector<int>>rounds) {
     vector<long>invest;
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n+1;i++)
     {
         invest.push_back(0);
     }
     for(int i=0;i<rounds.size();i++)
     {
         long l=rounds[i][0]-1;
-        long r=rounds[i][1]-1;
-        long amount=rounds[i][2];
-        for(int j=l;j<=r;j++)
-        {
-            invest[j]=invest[j]+amount;
-        }
+        long r=rounds[i][1];
+        long a=rounds[i][2];
+        invest[l]=invest[l]+a;
+        invest[r]=invest[r]-a;
+    }
+    for(int i=0;i<invest.size();i++)
+    {
+        invest[i]=invest[i-1]+invest[i];
     }
     return *max_element(invest.begin(),invest.end());
 }
